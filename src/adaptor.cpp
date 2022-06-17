242975c4175eea60275229beaedff7e9bd9fd879
shinnku-nikaidou <shinnkunikaidou@gmail.com>
2022-06-18 04:43:48 +0800

    init

---
+++ src/adaptor.cpp
@@ -0,0 +1,16 @@
+#ifndef _ADAPTOR_CPP
+#define _ADAPTOR_CPP
+
+#include <iostream>
+#include <algorithm>
+#include <utility> 
+#include <array>
+#include <vector>
+#include <tuple>
+#include <unordered_map>
+#include <exception>
+
+using std::vector;
+using std::operator""s;
+
+#endif /* _ADAPTOR_CPP */
---
+++ src/bipartite_graph.hpp
@@ -0,0 +1,66 @@
+#ifndef BIPARTITE_GRAPH_HPP
+#define BIPARTITE_GRAPH_HPP
+
+#include "adaptor.cpp"
+
+template <typename Nodes>
+concept nodeable = std::random_access_iterator<typename Nodes::iterator>;
+
+/*
+ * A bipartite graph, also called a bigraph, 
+ * is a set of graph vertices decomposed into two disjoint sets 
+ * such that no two graph vertices within the same set are adjacent. 
+ * A bipartite graph is a special case of a k-partite graph with k=2. 
+ * The illustration above shows some bipartite graphs, 
+ * with vertices in each graph colored based on to 
+ * which of the two disjoint sets they belong.
+ */
+template <typename Key, nodeable Nodes, typename Edges,
+          typename Alloc = std::allocator<Key>>
+requires(
+    std::same_as<typename Nodes::value_type, Key> &&std::same_as<
+        typename std::tuple_element<0, typename Edges::value_type>::type, Key>
+        &&std::same_as<
+            typename std::tuple_element<1, typename Edges::value_type>::type,
+            Key>) class bipartite_graph {
+public:
+  using value_type = Key;
+  using Edge = typename Edges::value_type;
+
+  Nodes U, V;
+  bipartite_graph() = delete;
+
+  /* 
+   * U contain all the nodes
+   */
+  bipartite_graph(const Nodes &U, const Nodes &V, const Edges &edges) {
+    this->U = U;
+    this->V = V;
+    size_t i = 0;
+    for (const auto &u : U) {
+      if (_key_to_index_U.contains(u)) {
+        throw std::invalid_argument("U Key should not contain the same value.");
+      } else {
+        _key_to_index_U[u] = i;
+        i++;
+        adjacency_list_of_u.push_back({});
+      }
+    }
+    i = 0;
+    for (const auto &v : V) {
+      if (_key_to_index_V.contains(v)) {
+        throw std::invalid_argument("V Key should not contain the same value.");
+      } else {
+        _key_to_index_V[v] = i;
+        i++;
+        adjacency_list_of_v.push_back({});
+      }
+    }
+  };
+
+private:
+  std::unordered_map<Key, size_t> _key_to_index_U, _key_to_index_V;
+  vector<vector<size_t>> adjacency_list_of_u, adjacency_list_of_v;
+};
+
+#endif // BIPARTITE_GRAPH_HPP
